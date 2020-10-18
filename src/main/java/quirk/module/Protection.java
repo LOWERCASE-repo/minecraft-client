34d0bb1ba052cb7225b0b24c0cf2fb0a954734fa
LOWERCASE <52083329+LOWERCASE-repo@users.noreply.github.com>
2020-10-18 19:09:32 -0400

    removed trigger bot

--- src/main/java/quirk/module/Protection.java
+++ src/main/java/quirk/module/Protection.java
@@ -2,6 +2,7 @@ package quirk.module;
 
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.mob.Monster;
+import net.minecraft.item.*;
 import net.minecraft.util.hit.EntityHitResult;
 import net.minecraft.util.hit.HitResult;
 import quirk.Quirk;
@@ -19,16 +20,17 @@ public class Protection {
             for (Entity entity : targets) entity.setGlowing(false);
             targets.clear();
         } else targets.removeIf(Objects::isNull);
-        for (Entity target : targets) target.setGlowing(true);
+        if (!Quirk.client.options.keyAttack.isPressed()) return;
         HitResult hit = Quirk.client.crosshairTarget;
         if (!(hit instanceof EntityHitResult)) return;
-        Entity entity = ((EntityHitResult) hit).getEntity();
-        if (Quirk.client.options.keyAttack.isPressed() || entity instanceof Monster) addTarget(entity);
-        if (!targets.contains(entity)) return;
-        Input.equipWeapon();
-        boolean charging = Quirk.client.player.getAttackCooldownProgress(0f) < 1f;
-        if (charging && Quirk.client.player.getPos().distanceTo(entity.getPos()) > 2f) return;
-        Input.press(Quirk.client.options.keyAttack);
+        Entity entity =((EntityHitResult) hit).getEntity();
+        addTarget(entity);
+//        if (!targets.contains(entity)) return;
+//        Input.equipWeapon();
+//        boolean charging = Quirk.client.player.getAttackCooldownProgress(0f) < 1f;
+//        if (charging && Quirk.client.player.getPos().distanceTo(entity.getPos()) > 2f) return;
+//        Quirk.client.options.keyAttack.setPressed(false);
+//        Input.press(Quirk.client.options.keyAttack);
     }
 
     void addTarget(Entity entity) {
